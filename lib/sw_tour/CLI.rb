ce92588c757441ed38b8b3349c3c4d8c06625bb7
Carrie Mathieu <carrieburks@gmail.com>
2021-03-25 14:20:32 -0400

    refactored CLI class, consolidated methods

--- lib/sw_tour/CLI.rb
+++ lib/sw_tour/CLI.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 class SwTour::CLI
     
-    # app flow
+    ## app flow
 
     # start
     def self.start
@@ -12,78 +12,72 @@ class SwTour::CLI
         starship
     end
 
-    # displays starship info
     def self.starship
+        # gets starship info from API
         SwTour::Starship.display_starships
         puts "\nPlease enter the number for your selected spaceship."
-        get_ship
-    end
 
-    # prompts user for input
-    def self.get_ship
+        # prompts user for input
         input = gets.chomp
         ship = input.to_i
-        display_starship_info(ship)
-    end
 
-    # checks to make sure number chosen is valid, directs to get_people
-    def self.display_starship_info(ship)
-        if ship <= SwTour::Starship.all.length && ship > 0
-            chosen_starship = SwTour::Starship.all[ship - 1]
-            chosen_starship.starship_info
-        else
-            puts "Please enter a valid input."
-            get_ship
+        # checks to make sure input is valid
+        until ship <= SwTour::Starship.all.length && ship > 0
+            puts "Please enter a valid number."
+            input = gets.chomp
+            ship = input.to_i
         end
+        
+        # gets info for starship from input
+        chosen_starship = SwTour::Starship.all[ship - 1]
+        chosen_starship.starship_info
+        
+        # allocates time to read starship info
         sleep(3)
         puts "\nYou get to pick a travel companion! Who would you like to take with you?"
         people
     end
 
-    # gets people info
     def self.people
+        # gets people info from API
         SwTour::People.display_people
-        get_people
-    end
 
-    # prompts user for input
-    def self.get_people
+        # prompts user for input
         input = gets.chomp
         person = input.to_i
-        display_people_info(person)
-    end
-    
-    # validates input
-    def self.display_people_info(person)
-        if person <= SwTour::People.all.length && person > 0
-            chosen_person = SwTour::People.all[person-1]
-            chosen_person.person_info
-        else
-            "Please enter a valid number:"
-            get_people
+
+        # checks to make sure input is valid
+        until person <= SwTour::People.all.length && person > 0
+            puts "Please enter a valid number."
+            input = gets.chomp
+            person = input.to_i
         end
+        
+        # gets info for person from input
+        chosen_person = SwTour::People.all[person-1]
+        chosen_person.person_info
+        
         planets
     end
 
     def self.planets
+        # gets planet info from API
         SwTour::Planet.display_planets
-        get_planet
-    end
-
-    def self.get_planet
+        
+        # gets planet input
         input = gets.chomp
         planet = input.to_i
-        display_planet_info(planet)
-    end
 
-    def self.display_planet_info(planet)
-        if planet <= SwTour::Planet.all.length && planet > 0
-            chosen_planet = SwTour::Planet.all[planet - 1]
-            chosen_planet.planet_info
-        else
-            puts "Please enter a valid input."
-            get_planet
+        # validates planet info
+        until planet <= SwTour::Planet.all.length && planet > 0
+            puts "Please enter a valid number."
+            input = gets.chomp
+            planet = input.to_i
         end
+
+        # gets info for planet from input
+        chosen_planet = SwTour::Planet.all[planet - 1]
+        chosen_planet.planet_info
     end
 
     def self.travel_again?
